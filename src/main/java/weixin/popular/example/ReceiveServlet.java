8f1cd3aa2bc37adadcc9c6a9302ab669976c7ee4
liyiorg <liyiorg@gmail.com>
2014-11-05 23:11:05 +0800

    add example

---
+++ src/main/java/weixin/popular/example/ReceiveServlet.java
@@ -0,0 +1,106 @@
+package weixin.popular.example;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import weixin.popular.bean.EventMessage;
+import weixin.popular.bean.xmlmessage.XMLTextMessage;
+import weixin.popular.util.ExpireSet;
+import weixin.popular.util.SignatureUtil;
+import weixin.popular.util.XMLConverUtil;
+
+/**
+ * 服务端事件消息接收
+ * @author Yi
+ *
+ */
+public class ReceiveServlet extends HttpServlet{
+
+    /**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+
+	//从官方获取
+	private String token = "test";
+
+    //重复通知过滤  时效60秒
+    private static ExpireSet<String> expireSet = new ExpireSet<String>(60);
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        ServletInputStream inputStream = request.getInputStream();
+        ServletOutputStream outputStream = response.getOutputStream();
+        String signature = request.getParameter("signature");
+        String timestamp = request.getParameter("timestamp");
+        String nonce = request.getParameter("nonce");
+        String echostr = request.getParameter("echostr");
+
+        //首次请求申请验证,返回echostr
+        if(echostr!=null){
+            outputStreamWrite(outputStream,echostr);
+            return;
+        }
+
+        //验证请求签名
+        if(!signature.equals(SignatureUtil.generateEventMessageSignature(token,timestamp,nonce))){
+            System.out.println("The request signature is invalid");
+            return;
+        }
+
+        if(inputStream!=null){
+            //转换XML
+            EventMessage eventMessage = XMLConverUtil.convertToObject(EventMessage.class,inputStream);
+            String expireKey = eventMessage.getFromUserName() + "__"
+            				   + eventMessage.getToUserName() + "__"
+            				   + eventMessage.getMsgId() + "__"
+            				   + eventMessage.getCreateTime();
+            if(expireSet.contains(expireKey)){
+            	//重复通知不作处理
+            	return;
+            }else{
+            	expireSet.add(expireKey);
+            }
+
+            //创建回复
+            XMLTextMessage xmlTextMessage = new XMLTextMessage(
+                    eventMessage.getFromUserName(),
+                    eventMessage.getToUserName(),
+                    "你好");
+            //回复
+            xmlTextMessage.outputStreamWrite(outputStream);
+            return;
+        }
+        outputStreamWrite(outputStream,"");
+    }
+
+    /**
+     * 数据流输出
+     * @param outputStream
+     * @param text
+     * @return
+     */
+    private boolean outputStreamWrite(OutputStream outputStream,String text){
+        try {
+            outputStream.write(text.getBytes("utf-8"));
+        } catch (UnsupportedEncodingException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}

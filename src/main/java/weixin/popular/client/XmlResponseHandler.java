dc002de8b2ee1948444dc0372fef37221721026a
liyi <liyiorg@gmail.com>
2014-12-29 13:29:44 +0800

    优化 ResponseHandler

--- src/main/java/weixin/popular/client/XmlResponseHandler.java
+++ src/main/java/weixin/popular/client/XmlResponseHandler.java
@@ -1,6 +1,8 @@
 package weixin.popular.client;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
@@ -12,22 +14,31 @@ import weixin.popular.util.XMLConverUtil;
 
 public class XmlResponseHandler{
 
-	public static <T> ResponseHandler<T> createResponseHandler(final Class<T> clazz){
-		return new ResponseHandler<T>() {
-			@Override
-			public T handleResponse(HttpResponse response)
-					throws ClientProtocolException, IOException {
-				int status = response.getStatusLine().getStatusCode();
-                if (status >= 200 && status < 300) {
-                    HttpEntity entity = response.getEntity();
-                    String str = EntityUtils.toString(entity);
-                   return XMLConverUtil.convertToObject(clazz,new String(str.getBytes("iso-8859-1"),"utf-8"));
-                } else {
-                    throw new ClientProtocolException("Unexpected response status: " + status);
-                }
+	private static Map<String, ResponseHandler<?>> map = new HashMap<String, ResponseHandler<?>>();
 
-			}
-		};
+	@SuppressWarnings("unchecked")
+	public static <T> ResponseHandler<T> createResponseHandler(final Class<T> clazz){
+		if(map.containsKey(clazz.getName())){
+			return (ResponseHandler<T>)map.get(clazz.getName());
+		}else{
+			ResponseHandler<T> responseHandler = new ResponseHandler<T>() {
+				@Override
+				public T handleResponse(HttpResponse response)
+						throws ClientProtocolException, IOException {
+					int status = response.getStatusLine().getStatusCode();
+	                if (status >= 200 && status < 300) {
+	                    HttpEntity entity = response.getEntity();
+	                    String str = EntityUtils.toString(entity);
+	                   return XMLConverUtil.convertToObject(clazz,new String(str.getBytes("iso-8859-1"),"utf-8"));
+	                } else {
+	                    throw new ClientProtocolException("Unexpected response status: " + status);
+	                }
+
+				}
+			};
+			map.put(clazz.getName(), responseHandler);
+			return responseHandler;
+		}
 	}
 
 }

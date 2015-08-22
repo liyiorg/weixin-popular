34d1e56253718e7fbc3e9c6dd16f76f81db5b249
mackjieson <mackjieson@gmail.com>
2015-08-22 23:37:49 +0800

    实现了微信开发平台授权后接口调用，通过code获取access_token。以及获取用户个人信息（UnionID机制）。

--- src/main/java/weixin/popular/api/TokenAPI.java
+++ src/main/java/weixin/popular/api/TokenAPI.java
@@ -4,6 +4,7 @@ import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.client.methods.RequestBuilder;
 
 import weixin.popular.bean.Token;
+import weixin.popular.bean.UserToken;
 import weixin.popular.client.LocalHttpClient;
 
 public class TokenAPI extends BaseAPI{
@@ -23,5 +24,46 @@ public class TokenAPI extends BaseAPI{
 				.build();
 		return LocalHttpClient.executeJsonResult(httpUriRequest,Token.class);
 	}
+	
+
+	
+	/**
+	 * 获取用户的token
+	 * @param appid
+	 * @param grant_type
+	 * @param code
+	 * @return
+	 */
+	public static UserToken usertoken(String appid,String secret,String code)
+	{
+		HttpUriRequest httpUriRequest = RequestBuilder.post()
+				.setUri(BASE_URI + "/sns/oauth2/access_token")
+				.addParameter("appid", appid)
+				.addParameter("secret", secret)
+				.addParameter("code", code)
+				.addParameter("grant_type", "authorization_code")
+				.build();
+		return LocalHttpClient.executeJsonResult(httpUriRequest,UserToken.class);
+	}
+	
+	
+	/**
+	 * 用户获取的二次token
+	 * @param appid
+	 * @param grant_type
+	 * @param refresh_token
+	 * @return
+	 */
+	public static UserToken refreshusertoken(String appid,String refresh_token)
+	{
+		HttpUriRequest httpUriRequest = RequestBuilder.post()
+				.setUri(BASE_URI + "/sns/oauth2/refresh_token")
+				.addParameter("appid", appid)
+				.addParameter("grant_type", "refresh_token")
+				.addParameter("refresh_token", refresh_token)
+				.build();
+		return LocalHttpClient.executeJsonResult(httpUriRequest,UserToken.class);
+	}
+
 
 }
---
+++ src/main/java/weixin/popular/bean/UserToken.java
@@ -0,0 +1,53 @@
+package weixin.popular.bean;
+
+public class UserToken extends BaseResult {
+	
+	private String access_token;
+	private int expires_in;
+	private String refresh_token;
+	private String openid;
+	private String scope;
+	private String unionid;
+	
+	
+	public String getUnionid() {
+		return unionid;
+	}
+	public void setUnionid(String unionid) {
+		this.unionid = unionid;
+	}
+	public String getAccess_token() {
+		return access_token;
+	}
+	public void setAccess_token(String access_token) {
+		this.access_token = access_token;
+	}
+	public int getExpires_in() {
+		return expires_in;
+	}
+	public void setExpires_in(int expires_in) {
+		this.expires_in = expires_in;
+	}
+	public String getRefresh_token() {
+		return refresh_token;
+	}
+	public void setRefresh_token(String refresh_token) {
+		this.refresh_token = refresh_token;
+	}
+	public String getOpenid() {
+		return openid;
+	}
+	public void setOpenid(String openid) {
+		this.openid = openid;
+	}
+	public String getScope() {
+		return scope;
+	}
+	public void setScope(String scope) {
+		this.scope = scope;
+	}
+	
+	
+	
+
+}

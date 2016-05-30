cff8072e456b74dfc68e5c47cb7d4b4df42d54a7
Moyq5 <moyq5_general@163.com>
2016-05-30 10:56:49 +0800

    开始开发“微信门店”类接口

--- src/main/java/weixin/popular/api/PoiAPI.java
+++ src/main/java/weixin/popular/api/PoiAPI.java
@@ -2,15 +2,13 @@ package weixin.popular.api;
 
 /**
  * 微信门店
- * @author
+ * @author Moyq5
  *
  */
 public class PoiAPI extends BaseAPI{
 
 	/**
-	 * 这里一片空白，开源贡献动起手来！
-	 * 
-	 * 参考其它API的开发方式
+	 * Moyq5　进行中...
 	 * 
 	 */
 }

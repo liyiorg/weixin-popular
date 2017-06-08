67922aedbeb28cd08b4023160a0fefdb5c2d3f83
rachel.li <541942463@qq.com>
2017-06-08 17:53:38 +0800

    添加原因返回字段
    添加原因返回字段

--- src/main/java/weixin/popular/bean/wxa/GetAuditstatusResult.java
+++ src/main/java/weixin/popular/bean/wxa/GetAuditstatusResult.java
@@ -5,6 +5,7 @@ import weixin.popular.bean.BaseResult;
 public class GetAuditstatusResult extends BaseResult {
 
 	private Integer status;
+	private String reason; // 当status=1，审核被拒绝时，返回的拒绝原因
 
 	public Integer getStatus() {
 		return status;
@@ -12,6 +13,13 @@ public class GetAuditstatusResult extends BaseResult {
 
 	public void setStatus(Integer status) {
 		this.status = status;
+	}
+	
+	public String getReason(){
+		return reason;
+	}
+	public void setReason(String reason){
+		this.reason = reason;
 	}
 
 }

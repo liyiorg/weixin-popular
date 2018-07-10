3505365aa630f9bd2f36bb4507abf21ef2c05710
chenxu <805128048@qq.com>
2018-07-10 15:13:05 +0800

    增加了一些无参构造器

--- src/main/java/weixin/popular/bean/message/message/MusicMessage.java
+++ src/main/java/weixin/popular/bean/message/message/MusicMessage.java
@@ -2,81 +2,86 @@ package weixin.popular.bean.message.message;
 
 public class MusicMessage extends Message {
 
-	private Music music;
+    public MusicMessage() {
+    }
+
+    private Music music;
 
 
-	public MusicMessage(String touser,Music music) {
-		super(touser,"music");
-		this.music = music;
-	}
+    public MusicMessage(String touser, Music music) {
+        super(touser, "music");
+        this.music = music;
+    }
 
-	public Music getMusic() {
-		return music;
-	}
+    public Music getMusic() {
+        return music;
+    }
 
-	public void setMusic(Music music) {
-		this.music = music;
-	}
+    public void setMusic(Music music) {
+        this.music = music;
+    }
 
-	public static class Music {
+    public static class Music {
 
-		private String title;
-		private String description;
-		private String musicurl;
-		private String hqmusicurl;
-		private String thumb_media_id;
+        private String title;
+        private String description;
+        private String musicurl;
+        private String hqmusicurl;
+        private String thumb_media_id;
 
+        public Music(){
 
+        }
 
-		public Music(String title, String description, String musicurl,
-				String hqmusicurl, String thumb_media_id) {
-			super();
-			this.title = title;
-			this.description = description;
-			this.musicurl = musicurl;
-			this.hqmusicurl = hqmusicurl;
-			this.thumb_media_id = thumb_media_id;
-		}
+        public Music(String title, String description, String musicurl,
+                     String hqmusicurl, String thumb_media_id) {
+            super();
+            this.title = title;
+            this.description = description;
+            this.musicurl = musicurl;
+            this.hqmusicurl = hqmusicurl;
+            this.thumb_media_id = thumb_media_id;
+        }
 
-		public String getTitle() {
-			return title;
-		}
+        public String getTitle() {
+            return title;
+        }
 
-		public void setTitle(String title) {
-			this.title = title;
-		}
+        public void setTitle(String title) {
+            this.title = title;
+        }
 
-		public String getDescription() {
-			return description;
-		}
+        public String getDescription() {
+            return description;
+        }
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
+        public void setDescription(String description) {
+            this.description = description;
+        }
 
-		public String getMusicurl() {
-			return musicurl;
-		}
+        public String getMusicurl() {
+            return musicurl;
+        }
 
-		public void setMusicurl(String musicurl) {
-			this.musicurl = musicurl;
-		}
+        public void setMusicurl(String musicurl) {
+            this.musicurl = musicurl;
+        }
 
-		public String getHqmusicurl() {
-			return hqmusicurl;
-		}
+        public String getHqmusicurl() {
+            return hqmusicurl;
+        }
 
-		public void setHqmusicurl(String hqmusicurl) {
-			this.hqmusicurl = hqmusicurl;
-		}
+        public void setHqmusicurl(String hqmusicurl) {
+            this.hqmusicurl = hqmusicurl;
+        }
 
-		public String getThumb_media_id() {
-			return thumb_media_id;
-		}
+        public String getThumb_media_id() {
+            return thumb_media_id;
+        }
 
-		public void setThumb_media_id(String thumbMediaId) {
-			thumb_media_id = thumbMediaId;
-		}
+        public void setThumb_media_id(String thumbMediaId) {
+            thumb_media_id = thumbMediaId;
+        }
 
-	}
+    }
 }
--- src/main/java/weixin/popular/bean/message/message/VideoMessage.java
+++ src/main/java/weixin/popular/bean/message/message/VideoMessage.java
@@ -4,67 +4,87 @@ package weixin.popular.bean.message.message;
  * 视频
  *
  * @author LiYi
- *
  */
 public class VideoMessage extends Message {
 
-	public VideoMessage(String touser,Video video) {
-		super(touser,"video");
-		this.video = video;
-	}
+    public VideoMessage() {
+    }
 
-	public Video video;
+    public VideoMessage(String touser, Video video) {
+        super(touser, "video");
+        this.video = video;
+    }
 
-
-
-	public Video getVideo() {
-		return video;
-	}
+    public Video video;
 
 
-
-	public void setVideo(Video video) {
-		this.video = video;
-	}
-
+    public Video getVideo() {
+        return video;
+    }
 
 
-	public static class Video {
-		private String media_id;
-		private String title;
-		private String description;
+    public void setVideo(Video video) {
+        this.video = video;
+    }
 
-		public Video(String media_id, String title, String description) {
-			super();
-			this.media_id = media_id;
-			this.title = title;
-			this.description = description;
-		}
 
-		public String getMedia_id() {
-			return media_id;
-		}
+    public static class Video {
+        private String media_id;
+        private String title;
+        private String description;
+        private String thumb_media_id;
 
-		public void setMedia_id(String mediaId) {
-			media_id = mediaId;
-		}
+        public Video() {
 
-		public String getTitle() {
-			return title;
-		}
+        }
 
-		public void setTitle(String title) {
-			this.title = title;
-		}
+        public Video(String media_id, String title, String description, String thumb_media_id) {
+            super();
+            this.media_id = media_id;
+            this.thumb_media_id = thumb_media_id;
+            this.title = title;
+            this.description = description;
+        }
 
-		public String getDescription() {
-			return description;
-		}
+        public Video(String media_id, String title, String description) {
+            super();
+            this.media_id = media_id;
+            this.title = title;
+            this.description = description;
+        }
 
-		public void setDescription(String description) {
-			this.description = description;
-		}
+        public String getMedia_id() {
+            return media_id;
+        }
 
-	}
+        public void setMedia_id(String mediaId) {
+            media_id = mediaId;
+        }
+
+        public String getThumb_media_id() {
+            return thumb_media_id;
+        }
+
+        public void setThumb_media_id(String thumb_media_id) {
+            this.thumb_media_id = thumb_media_id;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public void setTitle(String title) {
+            this.title = title;
+        }
+
+        public String getDescription() {
+            return description;
+        }
+
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+    }
 
 }

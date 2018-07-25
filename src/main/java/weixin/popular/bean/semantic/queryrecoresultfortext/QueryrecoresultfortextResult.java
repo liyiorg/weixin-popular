3007727777c5700e5b759ee5c61210c4e3a97b24
chenxu <805128048@qq.com>
2018-07-25 14:35:19 +0800

    新增微信智能接口

---
+++ src/main/java/weixin/popular/bean/semantic/queryrecoresultfortext/QueryrecoresultfortextResult.java
@@ -0,0 +1,21 @@
+package weixin.popular.bean.semantic.queryrecoresultfortext;
+
+import weixin.popular.bean.BaseResult;
+
+/**
+ * @ProjectName weixin-popular
+ * @Author: zeroJun
+ * @Date: 2018/7/25 10:11
+ * @Description:
+ */
+public class QueryrecoresultfortextResult extends BaseResult {
+    private String result;
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+}

a4b85d770897cb7a8b9b46da514fb13505161123
liyi <liuemc@msn.com>
2018-08-29 13:35:08 +0800

    #175 XMLConverUtil 多线程问题修复

--- src/main/java/weixin/popular/util/XMLConverUtil.java
+++ src/main/java/weixin/popular/util/XMLConverUtil.java
@@ -32,72 +32,86 @@ import com.sun.xml.bind.marshaller.CharacterEscapeHandler;
 
 /**
  * XML 数据接收对象转换工具类
+ * 
  * @author LiYi
  *
  */
-public abstract class XMLConverUtil{
-	
+public abstract class XMLConverUtil {
+
 	private static Logger logger = LoggerFactory.getLogger(XMLConverUtil.class);
 
-	private static Map<Class<?>, Marshaller> M_MAP;
+	private static Map<Class<?>, JAXBContext> JAXB_CONTEXT_MAP;
 
-	private static Map<Class<?>, Unmarshaller> U_MAP;
-	
 	static {
-		M_MAP = new ConcurrentHashMap<Class<?>, Marshaller>(200);
-		U_MAP = new ConcurrentHashMap<Class<?>, Unmarshaller>(200);
-	}
-	
-	/**
-	 * XML to Object
-	 * @param <T> T
-	 * @param clazz clazz
-	 * @param xml xml
-	 * @return T
-	 */
-	public static <T> T convertToObject(Class<T> clazz,String xml){
-		return convertToObject(clazz,new StringReader(xml));
+		JAXB_CONTEXT_MAP = new ConcurrentHashMap<Class<?>, JAXBContext>(256);
 	}
 
 	/**
 	 * XML to Object
-	 * @param <T> T
-	 * @param clazz clazz
-	 * @param inputStream  inputStream
+	 * 
+	 * @param <T>
+	 *            T
+	 * @param clazz
+	 *            clazz
+	 * @param xml
+	 *            xml
 	 * @return T
 	 */
-	public static <T> T convertToObject(Class<T> clazz,InputStream inputStream){
-		return convertToObject(clazz,new InputStreamReader(inputStream));
-	}
-	
-	/**
-	 * XML to Object
-	 * @param <T> T
-	 * @param clazz clazz
-	 * @param inputStream  inputStream
-	 * @param charset charset
-	 * @return T
-	 */
-	public static <T> T convertToObject(Class<T> clazz,InputStream inputStream,Charset charset){
-		return convertToObject(clazz,new InputStreamReader(inputStream, charset));
+	public static <T> T convertToObject(Class<T> clazz, String xml) {
+		return convertToObject(clazz, new StringReader(xml));
 	}
 
 	/**
 	 * XML to Object
-	 * @param <T> T
-	 * @param clazz clazz
-	 * @param reader reader
+	 * 
+	 * @param <T>
+	 *            T
+	 * @param clazz
+	 *            clazz
+	 * @param inputStream
+	 *            inputStream
+	 * @return T
+	 */
+	public static <T> T convertToObject(Class<T> clazz, InputStream inputStream) {
+		return convertToObject(clazz, new InputStreamReader(inputStream));
+	}
+
+	/**
+	 * XML to Object
+	 * 
+	 * @param <T>
+	 *            T
+	 * @param clazz
+	 *            clazz
+	 * @param inputStream
+	 *            inputStream
+	 * @param charset
+	 *            charset
+	 * @return T
+	 */
+	public static <T> T convertToObject(Class<T> clazz, InputStream inputStream, Charset charset) {
+		return convertToObject(clazz, new InputStreamReader(inputStream, charset));
+	}
+
+	/**
+	 * XML to Object
+	 * 
+	 * @param <T>
+	 *            T
+	 * @param clazz
+	 *            clazz
+	 * @param reader
+	 *            reader
 	 * @return T
 	 */
 	@SuppressWarnings("unchecked")
-	public static <T> T convertToObject(Class<T> clazz,Reader reader){
+	public static <T> T convertToObject(Class<T> clazz, Reader reader) {
 		try {
-			if(!U_MAP.containsKey(clazz)){
-				JAXBContext jaxbContext = JAXBContext.newInstance(clazz);
-				Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
-				U_MAP.put(clazz, unmarshaller);
+			if (!JAXB_CONTEXT_MAP.containsKey(clazz)) {
+				JAXB_CONTEXT_MAP.put(clazz, JAXBContext.newInstance(clazz));
 			}
-			return (T) U_MAP.get(clazz).unmarshal(reader);
+			Unmarshaller unmarshaller = JAXB_CONTEXT_MAP.get(clazz).createUnmarshaller();
+			return (T) unmarshaller.unmarshal(reader);
 		} catch (JAXBException e) {
 			logger.error("", e);
 		}
@@ -106,25 +120,26 @@ public abstract class XMLConverUtil{
 
 	/**
 	 * Object to XML
-	 * @param object object
+	 * 
+	 * @param object
+	 *            object
 	 * @return xml
 	 */
-	public static String convertToXML(Object object){
+	public static String convertToXML(Object object) {
 		try {
-			if(!M_MAP.containsKey(object.getClass())){
-				JAXBContext jaxbContext = JAXBContext.newInstance(object.getClass());
-				Marshaller marshaller = jaxbContext.createMarshaller();
-				marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
-				//设置CDATA输出字符
-				marshaller.setProperty(CharacterEscapeHandler.class.getName(), new CharacterEscapeHandler() {
-					public void escape(char[] ac, int i, int j, boolean flag, Writer writer) throws IOException {
-						writer.write(ac, i, j);
-					}
-				});
-				M_MAP.put(object.getClass(), marshaller);
+			if (!JAXB_CONTEXT_MAP.containsKey(object.getClass())) {
+				JAXB_CONTEXT_MAP.put(object.getClass(), JAXBContext.newInstance(object.getClass()));
 			}
+			Marshaller marshaller = JAXB_CONTEXT_MAP.get(object.getClass()).createMarshaller();
+			marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			// 设置CDATA输出字符
+			marshaller.setProperty(CharacterEscapeHandler.class.getName(), new CharacterEscapeHandler() {
+				public void escape(char[] ac, int i, int j, boolean flag, Writer writer) throws IOException {
+					writer.write(ac, i, j);
+				}
+			});
 			StringWriter stringWriter = new StringWriter();
-			M_MAP.get(object.getClass()).marshal(object,stringWriter);
+			marshaller.marshal(object, stringWriter);
 			return stringWriter.getBuffer().toString();
 		} catch (Exception e) {
 			logger.error("", e);
@@ -134,38 +149,41 @@ public abstract class XMLConverUtil{
 
 	/**
 	 * 转换简单的xml to map
-	 * @param xml xml
+	 * 
+	 * @param xml
+	 *            xml
 	 * @return map
 	 */
-	public static Map<String,String> convertToMap(String xml){
-		Map<String, String> map = new LinkedHashMap<String,String>();
+	public static Map<String, String> convertToMap(String xml) {
+		Map<String, String> map = new LinkedHashMap<String, String>();
 		try {
 			DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-			
+
 			/*
 			 * 避免 XXE 攻击
-			 * @since 2.8.21 
+			 * 
+			 * @since 2.8.21
 			 */
 			dbf.setFeature("http://apache.org/xml/features/disallow-doctype-decl", true);
 			dbf.setFeature("http://xml.org/sax/features/external-general-entities", false);
-	        dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
-	        dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
-	        dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-	        dbf.setXIncludeAware(false);
-	        dbf.setExpandEntityReferences(false);
-			
+			dbf.setFeature("http://xml.org/sax/features/external-parameter-entities", false);
+			dbf.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+			dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
+			dbf.setXIncludeAware(false);
+			dbf.setExpandEntityReferences(false);
+
 			DocumentBuilder db = dbf.newDocumentBuilder();
 			StringReader sr = new StringReader(xml);
 			InputSource is = new InputSource(sr);
 			Document document = db.parse(is);
 
 			Element root = document.getDocumentElement();
-			if(root != null){
+			if (root != null) {
 				NodeList childNodes = root.getChildNodes();
-				if(childNodes != null && childNodes.getLength()>0){
-					for(int i = 0;i < childNodes.getLength();i++){
-						Node node = childNodes.item(i); 
-						if( node != null && node.getNodeType() == Node.ELEMENT_NODE){
+				if (childNodes != null && childNodes.getLength() > 0) {
+					for (int i = 0; i < childNodes.getLength(); i++) {
+						Node node = childNodes.item(i);
+						if (node != null && node.getNodeType() == Node.ELEMENT_NODE) {
 							map.put(node.getNodeName(), node.getTextContent());
 						}
 					}
@@ -176,5 +194,5 @@ public abstract class XMLConverUtil{
 		}
 		return map;
 	}
-	
+
 }

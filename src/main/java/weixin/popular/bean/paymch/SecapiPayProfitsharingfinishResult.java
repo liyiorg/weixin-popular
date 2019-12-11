bc44ef13b3e733859bc095345891689fd5c4d6a3
liyi <li.yi@chinaott.net>
2019-12-11 16:01:42 +0800

    #210 修复

--- src/main/java/weixin/popular/bean/paymch/SecapiPayProfitsharingfinishResult.java
+++ src/main/java/weixin/popular/bean/paymch/SecapiPayProfitsharingfinishResult.java
@@ -1,5 +1,11 @@
 package weixin.popular.bean.paymch;
 
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "xml")
+@XmlAccessorType(XmlAccessType.FIELD)
 public class SecapiPayProfitsharingfinishResult extends MchBase {
 
 	private String transaction_id;

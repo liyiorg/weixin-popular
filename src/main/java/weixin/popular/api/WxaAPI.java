4db36812f43ba179daf6968d2ab60b313ea0745e
lihualing <lihualing@happyshare.com>
2019-12-20 12:31:53 +0800

    移除未使用的类引入

--- src/main/java/weixin/popular/api/WxaAPI.java
+++ src/main/java/weixin/popular/api/WxaAPI.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.util.List;
 
 import javax.imageio.ImageIO;
 

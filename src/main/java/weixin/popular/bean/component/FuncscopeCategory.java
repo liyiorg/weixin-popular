be2291c6d8f50128b3b4bf35b648b0e0153f2318
liyi <li.yi@chinaott.net>
2020-03-26 11:18:35 +0800

    2.8.31

---
+++ src/main/java/weixin/popular/bean/component/FuncscopeCategory.java
@@ -0,0 +1,14 @@
+package weixin.popular.bean.component;
+
+public class FuncscopeCategory {
+
+	private Integer id;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+}

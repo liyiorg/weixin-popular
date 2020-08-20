28d7500538fc15965132e61413acf6d34a15e97a
gaoyan <592238470@qq.com>
2020-08-20 15:04:02 +0800

    code格式化不一致，还原

--- src/main/java/weixin/popular/bean/paymch/AuthcodetoopenidResult.java
+++ src/main/java/weixin/popular/bean/paymch/AuthcodetoopenidResult.java
@@ -7,18 +7,15 @@ import javax.xml.bind.annotation.XmlRootElement;
 
 /**
  * 刷卡支付 授权码查询OPENID 返回数据
- * 
  * @author LiYi
  *
  */
 @XmlRootElement(name = "xml")
 @XmlAccessorType(XmlAccessType.FIELD)
-public class AuthcodetoopenidResult extends MchBase {
+public class AuthcodetoopenidResult extends MchBase{
 
 	@XmlElement
 	private String openid;
-	@XmlElement
-	private String sub_openid;
 
 	public String getOpenid() {
 		return openid;
@@ -27,12 +24,5 @@ public class AuthcodetoopenidResult extends MchBase {
 	public void setOpenid(String openid) {
 		this.openid = openid;
 	}
-
-	public String getSub_openid() {
-		return sub_openid;
-	}
-
-	public void setSub_openid(String sub_openid) {
-		this.sub_openid = sub_openid;
-	}
+	
 }
